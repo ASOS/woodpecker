ded0e53288d6497f48bb0ecda91c59d105872f7f
Ali Kheyrollahi <aliostad@gmail.com>
2016-06-02 15:29:29 +0100

    Updating the command sql

--- src/Woodpecker.Core/Sql/AzureSqlDvmExecutionStatsPecker.cs
+++ src/Woodpecker.Core/Sql/AzureSqlDvmExecutionStatsPecker.cs
@@ -15,7 +15,7 @@ SELECT
     DB_NAME() AS database_name, -- string
     CONCAT(@@SERVERNAME,'/',DB_NAME()) AS [full_name], -- string
     'procedure' AS sqldb_stats_type, -- string
-    COALESCE(OBJECT_NAME(object_id),'') AS sqldb_proc_object_name, -- string
+    COALESCE(CONCAT(OBJECT_SCHEMA_NAME(object_id),'.',OBJECT_NAME(object_id)),'') AS sqldb_proc_object_name, -- string
     type AS sqldb_stats_proc_type, -- string
     '' AS sqldb_stats_querystore_id,
     execution_count AS sqldb_stats_total_execution_count, -- string
@@ -28,27 +28,6 @@ SELECT
     '' AS sqldb_stats_query_hash
 FROM sys.dm_exec_procedure_stats
 WHERE OBJECT_NAME(object_id) IS NOT NULL
-UNION ALL
-SELECT
-    GETUTCDATE() AS collection_time_utc, -- datetime
-    @@SERVERNAME AS server_name, -- string
-    DB_NAME() AS database_name, -- string
-    CONCAT(@@SERVERNAME,'/',DB_NAME()) AS [full_name], -- string
-    'query' AS sqldb_stats_type, -- string
-    '' AS sqldb_proc_object_name, -- string
-    '' AS sqldb_stats_proc_type, -- string
-    COALESCE(qry.query_id,0) AS sqldb_stats_querystore_id, -- int64
-    execution_count AS sqldb_stats_total_execution_count, -- string
-    FLOOR(total_worker_time / 1000.0) AS sqldb_stats_total_cpu_time_ms, -- int64
-    total_elapsed_time AS sqldb_stats_total_elapsed_time_ms, -- int64
-    total_physical_reads AS sqldb_stats_total_physical_reads_ms, -- int64
-    total_logical_reads AS sqldb_stats_total_logical_reads_ms, -- int64
-    total_logical_writes AS sqldb_stats_total_logical_writes_ms, -- int64
-    CONVERT(VARCHAR(MAX),plan_handle,1) AS sqldb_stats_plan_handle, -- string
-    CONVERT(VARCHAR(MAX),qs.query_hash,1) AS sqldb_stats_query_hash
-FROM sys.dm_exec_query_stats qs
-LEFT JOIN sys.query_store_query qry
-    ON qry.query_hash = qs.query_hash
 ";
 
         protected override string GetQuery()
