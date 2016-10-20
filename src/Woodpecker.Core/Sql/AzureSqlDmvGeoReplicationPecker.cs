fc48b69296aa56aec121416c6413249196dbf71f
james duggan <james.duggan@asos.com>
2016-10-20 14:11:34 +0100

    add procedure status peck

--- src/Woodpecker.Core/Sql/AzureSqlDmvGeoReplicationPecker.cs
+++ src/Woodpecker.Core/Sql/AzureSqlDmvGeoReplicationPecker.cs
@@ -8,28 +8,28 @@ namespace Woodpecker.Core.Sql
 {
     public class AzureSqlDmvGeoReplicationPecker : AzureSqlDmvPeckerBase
     {
-        private const string DatabaseGeoReplicationInlineSqlNotSoBad = @"
-SELECT
-    GETUTCDATE() as collection_time_utc, -- datetime
-    @@SERVERNAME as server_name, -- string
-    DB_NAME() as database_name, -- string
-    CONCAT(@@SERVERNAME,'/',DB_NAME()) AS [full_name], -- string
-    partner_server AS sqldb_geo_partner_server, -- string
-    partner_database AS sqldb_geo_partner_database, -- string
-    CONCAT(partner_server,'/',partner_database) AS sqldb_geo_partner_full_name, -- string
-    replication_lag_sec AS sqldb_geo_lag_sec, -- int32
-    role_desc AS sqldb_geo_role_desc -- string
-FROM sys.dm_geo_replication_link_status
-";
+        private const string _query = @"
+select @@servername [collection_server_name]    
+     , db_name() [collection_database_name]    
+     , getutcdate() [collection_time_utc]    
+     , [partner_server] [secondary_server_name]  
+     , [partner_database] [secondary_database_name] 
+     , convert(datetime, [last_replication]) [last_replication]  
+     , [last_replication] 
+     , [replication_lag_sec]  
+     , [replication_state_desc]  
+     , [role_desc]  
+     , [secondary_allow_connections_desc]  
+from   sys.dm_geo_replication_link_status";
 
         protected override string GetQuery()
         {
-            return DatabaseGeoReplicationInlineSqlNotSoBad;
+            return _query;
         }
 
         protected override IEnumerable<string> GetRowKeyFieldNames()
         {
-            return new string[0];
+            return new[] {"collection_server_name", "collection_database_name", "partner_server", "partner_database" };
         }
 
         protected override string GetUtcTimestampFieldName()
