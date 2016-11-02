0639a559cef3646559ac54796b77ebd3728f9400
james duggan <james.duggan@asos.com>
2016-11-02 17:23:43 +0000

    create query store config pecker

---
+++ src/Woodpecker.Core/Sql/AzureSqlDmvQueryStoreConfigPecker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Woodpecker.Core.Sql
+{
+    public class AzureSqlDmvQueryStoreConfigPecker : AzureSqlDmvPeckerBase
+    {
+        private const string _query = @"
+select @@servername [collection_server_name]         
+     , db_name() [collection_database_name] 
+     , getutcdate() [collection_time_utc]          
+     , qso.[actual_state_desc]                      
+     , qso.[desired_state_desc]                     
+     , qso.[current_storage_size_mb]             
+     , qso.[max_storage_size_mb]                 
+     , qso.[readonly_reason]                     
+     , qso.[interval_length_minutes]             
+     , qso.[stale_query_threshold_days]             
+     , qso.[query_capture_mode_desc]                
+     , qso.[size_based_cleanup_mode_desc]           
+     , qso.[flush_interval_seconds]              
+     , qso.[max_plans_per_query]                 
+from   [sys].[database_query_store_options] qso;"; 
+
+        protected override string GetQuery()
+        {
+            return _query;
+        }
+
+        protected override IEnumerable<string> GetRowKeyFieldNames()
+        {
+           return new[] { "collection_server_name", "collection_database_name" };
+        }
+
+        protected override string GetUtcTimestampFieldName()
+        {
+            return "collection_time_utc";
+        }
+    }
+}
